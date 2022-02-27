a4a5a8044420a66d957f2b73d26fdc6ead304b6e
o10e1 <jmuszynski00@icloud.com>
2022-02-27 19:54:15 +0100

    First commit

---
+++ src/main/java/pl/o10e1/NetworkingBase/NetworkingBaseApplication.java
@@ -0,0 +1,13 @@
+package pl.o10e1.NetworkingBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkingBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkingBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/o10e1/NetworkingBase/NetworkingBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.o10e1.NetworkingBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkingBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
